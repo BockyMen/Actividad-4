64f10512681dc75ece75f7cd68b5b88374e3270c
BockyMen <129232559+BockyMen@users.noreply.github.com>
2026-02-25 18:26:44 -0500

    cambio

--- flutter_application_1/lib/widgets/CustomCard.dart
+++ flutter_application_1/lib/widgets/CustomCard.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class CustomCard extends StatelessWidget{
   final String title;
   final String subtitle;
--- flutter_application_1/lib/widgets/CustomHeader.dart
+++ flutter_application_1/lib/widgets/CustomHeader.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class Customheader extends StatelessWidget{
   final Image imagen;
   final String title;
