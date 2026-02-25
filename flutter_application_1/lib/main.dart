88d6f7c19b94088f23b4783fae796d9c6c3f48a8
BockyMen <129232559+BockyMen@users.noreply.github.com>
2026-02-25 11:00:26 -0500

    navegacion

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import './widgets/CustomCard.dart';
-import 'widgets/CustomHeader.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,80 +13,43 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("App Demo"),
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.star)),
+          title: const Text('AppBar Demo'),
           actions: <Widget>[
-            IconButton(onPressed: () {},
-              icon: Icon(Icons.menu)),
-            IconButton(onPressed: () {}, 
-              icon: Icon(Icons.circle)),
-            IconButton(onPressed: () {},
-             icon: Icon(Icons.circle_outlined)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.star)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.star)),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
+        body: const Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Center(
-                child: Text("Exporar Categorías", style: TextStyle(fontSize: 24, )),
+              CustomCard(
+                title: "hola",
+                subtitle: "adios",
+                icon: Icons.sailing_sharp,
               ),
-              SizedBox(height: 50),
-              Text("Usuario", style: TextStyle(fontSize: 24, ), textAlign: TextAlign.center),
-              SizedBox(height: 20),
-              Customheader(
-                imagen: Image(image: NetworkImage("https://cdn-icons-png.flaticon.com/512/3135/3135715.png")),
-                title: "Marcos Gomez",
-                subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-                icon: Icons.arrow_forward_ios,
+              CustomCard(
+                title: "hola",
+                subtitle: "adios",
+                icon: Icons.sailing_sharp,
               ),
-              SizedBox(height: 30),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text("Categorías", style: TextStyle(fontSize: 24)),
-                    SizedBox(height: 20),
-                    CustomCard(
-                      title: "Hola",
-                      subtitle: "gorditos",
-                      icon: Icons.abc,
-                    ),
-                    SizedBox(height: 12),
-                    CustomCard(
-                      title: "Hola",
-                      subtitle: "gorditos",
-                      icon: Icons.abc,
-                    ),
-                    SizedBox(height: 12),
-                    CustomCard(
-                      title: "Hola",
-                      subtitle: "gorditos",
-                      icon: Icons.abc,
-                    ),
-                  ],
-                ),
+              CustomCard(
+                title: "hola",
+                subtitle: "adios",
+                icon: Icons.sailing_sharp,
+              ),
+              CustomCard(
+                title: "hola",
+                subtitle: "adios",
+                icon: Icons.sailing_sharp,
               ),
             ],
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 1,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.grey,
           items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.circle), 
-              label: 'Home'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.circle), 
-              label: 'Buscar'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.circle),
-              label: 'Contactos',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.circle), 
-              label: 'Perfil'),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "Home"),
           ],
         ),
       ),
